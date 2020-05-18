ad6f01883c3430e8d7ebd713a770be0f2fc0aa4c
Denis <zendorx@gmail.com>
2020-05-18 21:00:52 +0300

    pure ecs select system

---
+++ Assets/Match3/Components/JewelSelected.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+public struct JewelSelected : IComponentData
+{
+    
+}
+
+public struct ClickPoint : IComponentData
+{
+    public float3 position;
+}
---
+++ Assets/Match3/Match3InputSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CodeMonkey.Utils;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+
+public class Match3InputSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            float3 clickPosition = UtilsClass.GetMouseWorldPosition();
+
+            var entity = World.EntityManager.CreateEntity(typeof(ClickPoint));
+            
+            World.EntityManager.SetComponentData(entity, new ClickPoint
+            {
+                position = clickPosition
+            });
+        }
+    }
+}
